42087785bdec3ad5cd3a4842dbea9dd36fa9d310
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-15 16:03:55 -0700

    Add SwashBuckle for Swagger api documentation generation Then change the launch path for the project to the test api

--- Program.cs
+++ Program.cs
@@ -5,9 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
