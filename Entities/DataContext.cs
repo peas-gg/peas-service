e63ecffc3b13628f55d4e3e754b5990104b6754d
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-15 15:46:39 -0700

    Add the DataContext and add it to services in the app builder

---
+++ Entities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PEAS.Entities
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to SqlServer database
+            options.UseSqlServer(Configuration.GetConnectionString("Database"));
+        }
+    }
+}
