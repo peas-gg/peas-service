c7090ef47b18752a264b2a3844e088a1293d904b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-20 15:55:58 -0700

    Add CreateBusiness model

---
+++ Models/Business/CreateBusiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PEAS.Models.Business
+{
+    public class CreateBusiness
+    {
+        public required string Sign { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public required string Color { get; set; }
+
+        public required string Description { get; set; }
+
+        public required Uri ProfilePhoto { get; set; }
+
+        public string? TwitterLink { get; set; }
+
+        public string? InstagramLink { get; set; }
+
+        public string? TikTokLink { get; set; }
+
+        public required double Latitude { get; set; }
+
+        public required double Longitude { get; set; }
+    }
+}
