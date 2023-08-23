4c89d89d0a078d06366fa3daba5cc3235181ad15
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-23 10:42:43 -0700

    Add Json String converter to the Device type enum

--- Entities/Authentication/Device.cs
+++ Entities/Authentication/Device.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace PEAS.Entities.Authentication
@@ -7,6 +8,7 @@ namespace PEAS.Entities.Authentication
     [Owned]
     public class Device
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public enum Type
         {
             apple,
