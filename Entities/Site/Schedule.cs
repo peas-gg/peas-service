6963cf3aae59c52001f0687a57ba0bf22f5dc89d
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-12 12:52:38 -0700

    Add Schedule module

---
+++ Entities/Site/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PEAS.Entities.Site
+{
+    public class Schedule
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public required Business Business { get; set; }
+
+        public required int DayOfWeek { get; set; }
+
+        public required int StartTime { get; set; }
+
+        public required int EndTime { get; set; }
+    }
+}
