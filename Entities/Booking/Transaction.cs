01c04dc3e33b6fd23d3aa5da038543209d0b77b9
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-13 15:31:05 -0700

    Add Transaction type and Transaction Status to transaction Add Wallet to the Site Entities

--- Entities/Booking/Transaction.cs
+++ Entities/Booking/Transaction.cs
@@ -1,12 +1,32 @@
-﻿using PEAS.Entities.Site;
+﻿using System.Text.Json.Serialization;
+using PEAS.Entities.Site;
 
 namespace PEAS.Entities.Booking
 {
     public class Transaction
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public enum Type
+        {
+            Deposit,
+            Withdrawal
+        }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public enum Status
+        {
+            Processing,
+            Succeeded,
+            Failed
+        }
+
         public int Id { get; set; }
 
         public required Business Business { get; set; }
+
+        public required Type TransactionType { get; set; }
+
+        public required Status TransactionStatus { get; set; }
 
         public required Order? Order { get; set; }
 
---
+++ Entities/Site/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PEAS.Entities.Site
+{
+    public class Wallet
+    {
+        public required Business Business { get; set; }
+
+        public required decimal Balance { get; set; }
+
+        public required decimal HoldBalance { get; set; }
+
+        [Timestamp]
+        public required byte[] Version { get; set; }
+    }
+}
