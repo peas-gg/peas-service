7b2683569398c15a19d585ad4576d70c407bb074
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-16 14:48:14 -0700

    Add DateRange and add a helper function for retrieving availability

---
+++ Helpers/Utilities/DateRange.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace PEAS.Helpers.Utilities
+{
+    public static partial class DateTimeExtensions
+    {
+        public static DateTime ResetTimeToStartOfDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, 0);
+        }
+        public static DateTime ResetTimeToEndOfDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day).AddDays(1).AddTicks(-1);
+        }
+    }
+
+    public interface IRange<T>
+    {
+        T Start { get; }
+        T End { get; }
+        bool WithInRange(T value);
+        bool WithInRange(IRange<T> range);
+    }
+
+    public class DateRange : IRange<DateTime>
+    {
+        public DateRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+            if (End < Start)
+            {
+                throw new Exception("Invalid date range");
+            }
+        }
+
+        public DateTime Start { get; private set; }
+
+        public DateTime End { get; private set; }
+
+        public bool WithInRange(DateTime value)
+        {
+            return (Start <= value) && (value <= End);
+        }
+
+        public bool WithInRange(IRange<DateTime> range)
+        {
+            return (Start <= range.Start) && (range.End <= End);
+        }
+
+        public static List<DateRange> GetAvailability(DateRange bookingDate, TimeSpan orderDuration, List<DateRange> bookings)
+        {
+            DateTime prevDate = bookingDate.Start;
+            DateTime currentDate = bookingDate.Start;
+            List<DateRange> availability = new();
+            while (currentDate < bookingDate.End)
+            {
+                currentDate += orderDuration;
+                var timeSlot = new DateRange(prevDate, currentDate);
+                if (!bookings.Any(x => x.WithInRange(timeSlot)))
+                {
+                    availability.Add(timeSlot);
+                }
+                prevDate = currentDate;
+            }
+            return availability;
+        }
+    }
+}
