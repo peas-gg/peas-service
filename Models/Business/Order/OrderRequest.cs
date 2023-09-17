cdeb76ff41ebd68ac728e7380913d497e4419d45
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-17 11:04:36 -0700

    Add OrderRequest model

---
+++ Models/Business/Order/OrderRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PEAS.Models.Business
+{
+	public class OrderRequest
+	{
+		public Guid BlockId { get; set; }
+		public DateTime Date { get; set; }
+		public required string FirstName { get; set; }
+		public required string Lastname { get; set; }
+		public required string Email { get; set; }
+		public required string Phone { get; set; }
+	}
+}
