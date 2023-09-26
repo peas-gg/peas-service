8bff16d246e76d242e4aa07ecabaf1b1e675677f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-26 08:16:21 -0700

    Add new Email folder under services for emails

---
+++ Services/Email/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PEAS.Services.Email
+{
+    public interface IEmailService
+    {
+
+    }
+
+    public class EmailService : IEmailService
+	{
+		public EmailService(IConfiguration configuration, ILogger<EmailService> logger)
+		{
+		}
+	}
+}
