8023ea00ec231c4ad8ba8e5d67435beb7bb96229
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-05 02:38:05 -0700

    Create PaymentIntent when the user requests a payment or update the existing one

--- Services/PaymentService.cs
+++ Services/PaymentService.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PEAS.Entities;
 using PEAS.Entities.Booking;
+using PEAS.Entities.Site;
 using PEAS.Helpers;
+using Stripe;
 
 namespace PEAS.Services
 {
     public interface IPaymentService
     {
-        string RequestPayment(Guid orderId);
+        string StartPayment(Guid orderId, int tip);
     }
 
     public class PaymentService : IPaymentService
@@ -16,44 +18,101 @@ namespace PEAS.Services
         private readonly IPushNotificationService _pushNotificationService;
         private readonly ILogger<PaymentService> _logger;
 
-        public PaymentService(DataContext context, IPushNotificationService pushNotificationService, ILogger<PaymentService> logger)
+        public PaymentService(IConfiguration configuration, DataContext context, IPushNotificationService pushNotificationService, ILogger<PaymentService> logger)
         {
             _context = context;
             _pushNotificationService = pushNotificationService;
             _logger = logger;
+            StripeConfiguration.ApiKey = configuration.GetSection("Stripe").Value ?? "";
+
         }
 
-        public string RequestPayment(Guid orderId)
+        public string StartPayment(Guid orderId, int tip)
         {
             try
             {
                 Order? order = _context.Orders.Include(x => x.Payment).First(x => x.Id == orderId);
-                if (order == null || !order.DidRequestPayment)
+
+                if (order == null)
                 {
                     throw new AppException("Invalid OrderId");
                 }
 
-                if (order.Payment?.Total >= order.Price)
+                if (!order.DidRequestPayment)
                 {
-                    throw new AppException("Order has been paid for");
+                    throw new AppException("Payment has not been requested for this order");
                 }
+                
+                var paymentIntentService = new PaymentIntentService();
 
                 if (order.Payment != null)
                 {
-                    //Return PaymentIntent if it exists
-                    return order.Payment.PaymentIntentId;
+                    if (order.Payment.Total >= order.Price)
+                    {
+                        throw new AppException("Order has been paid for");
+                    }
+
+                    //Update the payment intent to match the latest order price then return the client secret
+                    var options = new PaymentIntentUpdateOptions
+                    {
+                        Amount = getPaymentIntentAmount(order, tip),
+                        Metadata = new Dictionary<string, string>
+                        {
+                            { "tip", $"{tip}" },
+                        },
+                    };
+
+                    PaymentIntent paymentIntent = paymentIntentService.Update(order.Payment.PaymentIntentId, options);
+                    return paymentIntent.ClientSecret;
                 }
                 else
                 {
                     //Create PaymentIntent if it does not exist
-                    return "";
+                    var paymentIntentOptions = new PaymentIntentCreateOptions
+                    {
+                        Amount = getPaymentIntentAmount(order, tip),
+                        Currency = Currency.CAD.ToString().ToLower(),
+                        Metadata =
+                        {
+                            {"orderId", $"{order.Id}"},
+                            {"tip", $"{tip}"}
+                        },
+                        AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                        {
+                            Enabled = true,
+                        },
+                    };
+
+                    PaymentIntent paymentIntent = paymentIntentService.Create(paymentIntentOptions);
+
+                    //Update Order
+                    order.Payment = new Payment
+                    {
+                        PaymentIntentId = paymentIntent.Id,
+                        Base = 0,
+                        Deposit = 0,
+                        Tip = 0,
+                        Fee = 0,
+                        Total = 0,
+                        Created = DateTime.UtcNow
+                    };
+
+                    _context.Update(order);
+                    _context.SaveChanges();
+
+                    return paymentIntent.ClientSecret;
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 AppLogger.Log(_logger, e);
                 throw AppException.ConstructException(e);
             }
+        }
+
+        private int getPaymentIntentAmount(Order order, int tip)
+        {
+            return order.Price + tip;
         }
     }
 }
