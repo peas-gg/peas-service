ca3b4c0d64055c578de86b4c06ea2647d229e32c
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-05 22:26:26 -0700

    Delete Transaction entity Delete Wallet entity Add Withdrawal entity

---
+++ Entities/Site/Withdrawal.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PEAS.Entities.Site
+{
+    public class Withdrawal
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public enum Status
+        {
+            Pending,
+            Succeeded,
+            Failed
+        }
+
+        public required Business Business { get; set; }
+
+        public required int Amount { get; set; }
+
+        public required Status WithdrawalStatus { get; set; }
+
+        public required DateTime Created { get; set; }
+
+        public required DateTime? Completed { get; set; }
+        
+        [Timestamp]
+        public required byte[] Version { get; set; }
+    }
+}
