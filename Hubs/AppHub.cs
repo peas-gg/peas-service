4c48d4cdaedd62279a6810233064624a17e6d946
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-10 14:24:33 -0700

    Add AzureSignalR to the project then add the AppHub

---
+++ Hubs/AppHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PEAS.Entities;
+
+namespace PEAS.Hubs
+{
+    public class AppHub : Hub
+    {
+        private readonly ILogger<AppHub> _logger;
+        private readonly DataContext _dataContext;
+
+        public AppHub(ILogger<AppHub> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+    }
+}
