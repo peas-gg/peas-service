9febb91823f05a399513ee0cf37d0d9cbbae56da
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-10 23:11:59 -0700

    Add Apphub functions for subscribing for updates with a user, unsubscribing and sending OrderRecevied and PaymentReceived methods as well

--- Hubs/AppHub.cs
+++ Hubs/AppHub.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using PEAS.Entities;
+using PEAS.Entities.Authentication;
+using PEAS.Entities.Booking;
+using PEAS.Helpers;
+using PEAS.Helpers.Utilities;
 
 namespace PEAS.Hubs
 {
@@ -12,6 +16,64 @@ namespace PEAS.Hubs
         {
             _logger = logger;
             _dataContext = dataContext;
+        }
+
+        public async Task Subscribe()
+        {
+            try
+            {
+                Account? account = (Account?)Context.GetHttpContext()?.Items["Account"];
+                if (account == null)
+                {
+                    await Clients.Client(Context.ConnectionId).SendAsync("UnAuthorized");
+                }
+                else
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, account.Id.ToString());
+                }
+            }
+            catch(Exception e)
+            {
+                AppLogger.Log(_logger, e);
+            }
+        }
+
+        public async Task UnSubscribe(Guid accountId)
+        {
+            try
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, accountId.ToString());
+            }
+            catch(Exception e)
+            {
+                AppLogger.Log(_logger, e);
+            }
+        }
+
+        public void OrderReceived(Order order, Account account)
+        {
+            try
+            {
+                string message = $"Reservation request from {order.Customer.FirstName} {order.Customer.LastName} for {order.Title}";
+                Clients.User(account.Id.ToString()).SendAsync("OrderReceived", message);
+            }
+            catch(Exception e)
+            {
+                AppLogger.Log(_logger, e);
+            }
+        }
+
+        public void PaymentReceived(Order order, Account account)
+        {
+            try
+            {
+                string message = $"Payment received from {order.Customer.FirstName} {order.Customer.LastName} (${Price.Format(order.Payment!.Total)})";
+                Clients.User(account.Id.ToString()).SendAsync("PaymentReceived", message);
+            }
+            catch(Exception e)
+            {
+                AppLogger.Log(_logger, e);
+            }
         }
     }
 }
