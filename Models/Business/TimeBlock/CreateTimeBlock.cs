b1e675b8fa04efc155a6f7a060404e579d681d50
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2024-01-08 09:49:15 -0600

    Add CreateTimeBlock model Add TimeBlockResponse model

---
+++ Models/Business/TimeBlock/CreateTimeBlock.cs
@@ -0,0 +1,10 @@
+﻿namespace PEAS.Models.Business.TimeBlock
+{
+    public class CreateTimeBlock
+    {
+        public required Guid BusinessId { get; set; }
+        public required string Title { get; set; }
+        public required DateTime StartTime { get; set; }
+        public required DateTime EndTime { get; set; }
+    }
+}
---
+++ Models/Business/TimeBlock/TimeBlockResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PEAS.Models.Business.TimeBlock
+{
+    public class TimeBlockResponse
+    {
+        public Guid Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required DateTime StartTime { get; set; }
+
+        public required DateTime EndTime { get; set; }
+    }
+}
