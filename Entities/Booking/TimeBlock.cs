4932af82674a00a3b8acd107bff2cfc9dec185b8
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2024-01-08 08:35:16 -0600

    Add TimeBlock Entity

---
+++ Entities/Booking/TimeBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using PEAS.Entities.Site;
+
+namespace PEAS.Entities.Booking
+{
+    [Owned]
+    public class TimeBlock
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public required Business Business { get; set; }
+
+        public required string Title { get; set; }
+
+        public required DateTime StartTime { get; set; }
+
+        public required DateTime EndTime { get; set; }
+    }
+}
